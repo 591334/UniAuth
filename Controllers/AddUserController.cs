6c89937e52588551a346ea2f58608abbe377d191
Henrik Engdal <69847575+591334@users.noreply.github.com>
2023-09-18 01:33:08 +0200

    Add

--- Controllers/AddUserController.cs
+++ Controllers/AddUserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
 using System.Text;
 using UniAuth.Models;
 
@@ -14,98 +16,99 @@ namespace UniAuth.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddNewUser(FormCollection form)
+        public async Task<IActionResult> AddNewUser(IFormCollection form)
         {
+            IdentityResponse ir = new IdentityResponse();
+            string access_token = ir.access_token;
+
+            try
             {
-                try
+                string name = form["Name"];
+                string addressLine1 = form["addressLine1"];
+                string addressLine2 = form["addressLine2"];
+                string addressLine3 = form["addressLine3"];
+                string city = form["city"];
+                string countrycode = form["countrycode"];
+                string postalcode = form["postalcode"];
+                string phoneCountryCode = form["phoneCountryCode"];
+                string type = form["type"];
+                string mobileNumber = form["mobilenumber"];
+                string emailAddress = form["emailAddress"];
+                string comment = form["floatingTextarea2"];
+
+                var payload = new
                 {
-                    // Get the form data from the submitted form
-                    string name = form["Name"];
-                    string addressLine1 = form["addressLine1"];
-                    string addressLine2 = form["addressLine2"];
-                    string addressLine3 = form["addressLine3"];
-                    string city = form["city"];
-                    string countrycode = form["countrycode"];
-                    string postalcode = form["postalcode"];
-                    string phoneCountryCode = form["phoneCountryCode"];
-                    string type = form["type"];
-                    string mobileNumber = form["mobilenumber"];
-                    string emailAddress = form["emailAddress"];
-                    string comment = form["floatingTextarea2"];
-
-                    // Now you can use these variables to construct the payload
-                    var payload = new
+                    Info = new
                     {
-                        Info = new
+                        Name = name,
+                        InvoiceAddress = new
                         {
-                            Name = name,
-                            InvoiceAddress = new
-                            {
-                                AddressLine1 = addressLine1,
-                                AddressLine2 = addressLine2,
-                                AddressLine3 = addressLine3,
-                                City = city,
-                                CountryCode = countrycode,
-                                PostalCode = postalcode,
-                            },
-                            DefaultPhone = new
-                            {
-                                CountryCode = phoneCountryCode,
-                                Description = type,
-                                Number = mobileNumber,
-                            },
-                            DefaultEmail = new
-                            {
-                                EmailAddress = emailAddress,
-                            }
+                            AddressLine1 = addressLine1,
+                            AddressLine2 = addressLine2,
+                            AddressLine3 = addressLine3,
+                            City = city,
+                            CountryCode = countrycode,
+                            PostalCode = postalcode,
                         },
-                        Comment = comment
-                    };
+                        DefaultPhone = new
+                        {
+                            CountryCode = phoneCountryCode,
+                            Description = type,
+                            Number = mobileNumber,
+                        },
+                        DefaultEmail = new
+                        {
+                            EmailAddress = emailAddress,
+                        }
+                    },
+                    Comment = comment
+                };
 
-                    // Convert the payload to JSON
-                    var payloadJson = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
+                var payloadJson = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
+                var apiEndpoint = "https://test-api.softrig.com/api/biz/contacts";
 
-                    // Define the API endpoint URL
-                    var apiEndpoint = "https://test-api.softrig.com/api/biz/contacts";
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", access_token);
+                    httpClient.DefaultRequestHeaders.Add("CompanyKey", "91aec9da-f8c0-4fdd-a2a4-a4aeb82275c3");
+                    httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
 
-                    // Create an HttpClient instance
-                    using (var httpClient = new HttpClient())
+                    var jwtSecurityToken = new JwtSecurityToken(access_token);
+
+                    if (jwtSecurityToken.Payload.TryGetValue("AppFramework", out var baseUrl))
                     {
-                        // Set the content type header
-                        httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
-
                         try
                         {
-                            // Send the POST request and await the response
                             var response = await httpClient.PostAsync(apiEndpoint, new StringContent(payloadJson, Encoding.UTF8, "application/json"));
 
-                            // Check the response status code
                             if (response.IsSuccessStatusCode)
                             {
-                                // Handle the successful response
                                 var responseContent = await response.Content.ReadAsStringAsync();
-                                // Process the response content as needed
                                 return Content(responseContent);
                             }
                             else
                             {
-                                // Handle the error response
                                 var errorMessage = $"API request failed with status code {response.StatusCode}";
                                 return Content(errorMessage);
                             }
                         }
                         catch (Exception ex)
                         {
-                            // Handle any exceptions that occur during the request
                             return Content($"An error occurred: {ex.Message}");
                         }
                     }
-                }
-                catch (Exception ex)
-                {
-                    // Handle any exceptions that occur during form data processing
-                    return Content($"An error occurred: {ex.Message}");
+                    else
+                    {
+                        return Content("AppFramework value not found in the JWT payload.");
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                return Content($"An error occurred: {ex.Message}");
+            }
+            return Content("An unexpected error occurred.");
+        }
+    }
     }
 }
--- Controllers/MyContactsController.cs
+++ Controllers/MyContactsController.cs
@@ -26,75 +26,17 @@ namespace UniAuth.Controllers
 
             var httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", access_token);
+            httpClient.DefaultRequestHeaders.Add("CompanyKey", "91aec9da-f8c0-4fdd-a2a4-a4aeb82275c3");
 
-            // Request api data
-            /*
-            try
-            {
 
-                var jwtSecurityToken = new JwtSecurityToken(access_token);
-                if (jwtSecurityToken.Payload.TryGetValue("AppFramework", out var baseUrl))
-                {
-                    Console.WriteLine($"Base url is {baseUrl}");
-                    var apiResult = await httpClient.GetAsync(new Uri(baseUrl + "api/biz/companysettings"));
-
-                    return Ok(await apiResult.Content.ReadAsStringAsync());
-                }
-                else
-                {
-                    return BadRequest($"Could not find claim on token");
-                }
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-
-            return BadRequest("this is wrong");*/
-
-        // Set as list
-
-        
             List<string> itemList = new List<string>
             {
                 "Item 1",
                 "Item 2",
                 "Item 3"
-                // Add more items as needed
             };
         
             return View(itemList);
         }
-
-        /*
-        [HttpGet]
-        private async Task<IActionResult> GetListOfContacts()
-        {
-            string accessToken = IdentityResponse.access_token;
-            var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            httpClient.DefaultRequestHeaders.Add("CompanyKey", "91aec9da-f8c0-4fdd-a2a4-a4aeb82275c3");
-
-            try
-            {
-                var jwtSecurityToken = new JwtSecurityToken(accessToken);
-                if (jwtSecurityToken.Payload.TryGetValue("AppFramework", out var baseUrl))
-                {
-                    Console.WriteLine($"Base url is {baseUrl}");
-                    var apiResult = await httpClient.GetAsync(new Uri(baseUrl + "api/biz/contacts"));
-                    return Ok(await apiResult.Content.ReadAsStringAsync());
-                }
-                else
-                {
-                    return BadRequest($"Could not find claim on token");
-                }
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            return BadRequest("this is wrong");
-        }*/
-
     }
 }
